1912d812ccba05fe906de8e3a7c0f4b9c45519dc
fanfanfw <firgiawan.fanfan@gmail.com>
2023-05-26 15:22:54 +0700

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val text: String? = "raknum"
+
+//val textLength = text.length // compile time error
+
+    if (text  null){
+        val textLength = text.length // ready to go
+    }
+}
